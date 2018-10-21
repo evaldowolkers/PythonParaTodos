b6df7add9c56747caf491c78a55a7d391882ddbd
Evaldo Wolkers <evaldowolkers@gmail.com>
2018-10-21 15:45:17 -0300

    Modificado exemplo da seção 35, aula 01, para não exibir a senha quando digitada.

--- Secao35-TrabalhandoComE-mail/Aula01_EnviandoEmailsSimples/codigo/exemplo.py
+++ Secao35-TrabalhandoComE-mail/Aula01_EnviandoEmailsSimples/codigo/exemplo.py
@@ -1,7 +1,8 @@
 import send_mail_gmail as smg
+import getpass
 
 de = input("Informe seu usuário do GMail: ")
-senha = input('Informe sua senha do GMail: ')
+senha = getpass.getpass('Informe sua senha do GMail: ')
 para = input('Informe o destinatário (ou vários separados por vírgula): ')
 assunto = input("Informe o assunto/título do e-mail: ")
 mensagem = input("Escreva a mensagem: ")
