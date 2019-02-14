c9d044c4ad69ac7c11b7348f99a70887cd079ecb
Evaldo Wolkers <evaldowolkers@gmail.com>
2019-02-14 20:21:56 -0200

    Adicionados arquivos da seção 4, aula desafios diversos.

---
+++ Secao04-ConceitosBasicos/Aula05_DesafioConceitosBasicos/codigo/basico.py
@@ -0,0 +1,5 @@
+print("O resultado de 10 + 25 é:", 10 + 25)
+print("O resultado de 20 - 15 é:", 20 - 15)
+print("O resultado de 18 * 2 é:", 18 * 2)
+print("O resultado de 58 / 3 é:", 58 / 3)
+print("O resultado de 58 // 3 é:", 58 // 3)
