461ceaf15b5d05e2c60b0619fe376e5ad6624258
evaldowolkers <evaldowolkers@gmail.com>
2019-04-20 18:21:20 -0300

    Atualizados arquivos da seção 04, aulas 3, 4 e 5.

---
+++ Secao04-ConceitosBasicos/Aula04_OperacoesMatematicas/codigo/operacoes_matematicas.py
@@ -0,0 +1,26 @@
+"""
+    Operações matemáticas
+"""
+
+valor1 = 25
+valor2 = 10
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+divisao_inteiro = valor1 // valor2
+resto_divisao_valor1_valor2 = valor1 % valor2
+valor1_elevado_valor2 = valor1 ** valor2
+valor1_raiz_quadrada = valor1 ** (1/2)
+valor2_raiz_cubica = valor2 ** (1/3)
+
+print("Soma -> valor1 + valor2:", soma)
+print("Subtração -> valor1 - valor2:", subtracao)
+print("Multiplicação -> valor1 * valor2:", multiplicacao)
+print("Divisão -> valor1 / valor2:", divisao)
+print("Divisão inteira -> valor1 // valor2:", divisao_inteiro)
+print("Módulo -> valor1 % valor2:", resto_divisao_valor1_valor2)
+print("Potenciação -> valor1 ** valor2:", valor1_elevado_valor2)
+print("Radiciação -> valor1 ** (1/2):", valor1_raiz_quadrada)
+print("Radiciação -> valor2 ** (1/3):", valor2_raiz_cubica)
