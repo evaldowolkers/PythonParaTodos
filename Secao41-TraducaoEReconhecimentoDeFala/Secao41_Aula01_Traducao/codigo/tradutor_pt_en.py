9d6b32fd2f82fbfff28f8bb7ecf1df2b0b5b217f
evaldowolkers <evaldowolkers@gmail.com>
2019-04-21 00:04:39 -0300

    Corrigindo alguns paths.

--- Secao41-TraducaoEReconhecimentoDeFala/Secao41_Aula01_Traducao/codigo/tradutor_pt_en.py
+++ Secao41-TraducaoEReconhecimentoDeFala/Secao41_Aula01_Traducao/codigo/tradutor_pt_en.py
@@ -1,13 +1,7 @@
 from googletrans import Translator
-from googletrans.gtoken import TokenAcquirer
 
 texto_pt = input("Escreva uma frase: ")
-
 translator = Translator()
-acquirer = TokenAcquirer()
-acquirer.do(texto_pt)
-
 texto_en = translator.translate(texto_pt, src="pt", dest="en")
-
 print("Texto original:", texto_pt)
 print("Tradução:", texto_en.text)
